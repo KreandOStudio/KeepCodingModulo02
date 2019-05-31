43a8ad3abe498b5bdaac34754f72d8c44f3e2384
KreandOStudio <rubenalvarezguerrero@gmail.com>
2019-05-31 21:10:57 +0200

    Pos algo mas 2

---
+++ poo1.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otra_tortuguita = turtle.Turtle()
+
+tortuguita.fd(50)
+tortuguita.shape("turtle")
+otra_tortuguita.color("red")
+otra_tortuguita.left(90)
+otra_tortuguita.fd(50)
+input()
