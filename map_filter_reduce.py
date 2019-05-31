43ba2835d8f842d67ff1d16030400ed97785b5b5
KreandOStudio <rubenalvarezguerrero@gmail.com>
2019-05-31 20:52:35 +0200

    Pos algo mas

---
+++ map_filter_reduce.py
@@ -0,0 +1,8 @@
+lista = [1, 3, -1, 15, 9]
+
+lista_dobles = map(lambda x: x*2, lista)
+
+lis_dob = map(lambda x: x*2, lista)
+
+print("Lista dobles?: {}".format(lista_dobles))
+print("Yo que se: {}".format(lis_dob))
